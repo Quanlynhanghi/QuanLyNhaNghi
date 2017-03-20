cad31b5e0b212572b0b6a9711c3710de03868cc3
ThiDao <thi.daoba@gmail.com>
2017-03-20 08:33:50 +0700

    Lần 2

---
+++ FrmMain/HeThong/Frm_DangNhap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FrmMain.HeThong
+{
+    public partial class Frm_DangNhap : Form
+    {
+        public Frm_DangNhap()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FrmMain/cls_Main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FrmMain
+{
+    public class cls_Main
+    {
+        public static string duongdanfileketnoi = Application.StartupPath + @"\connec.ini";
+    }
+}
